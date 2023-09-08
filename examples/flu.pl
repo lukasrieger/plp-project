04dc4a551278ebc301997048c3b545b99276d263
Muhlex <21311428+Muhlex@users.noreply.github.com>
2023-09-08 19:30:50 +0200

    Fully handle list-free bodies and convert examples

--- examples/flu.pl
+++ examples/flu.pl
@@ -1,12 +1,10 @@
 :- style_check(-singleton).
 :- use_module(sampler).
 
-0.6::epidemic <--- [flu(X), cold].
-0.3::pandemic <--- [flu(X), cold].
+0.6::epidemic <--- flu(X), cold.
+0.3::pandemic <--- flu(X), cold.
 
-
-0.7::cold <--- [].
+0.7::cold <--- .
 
 flu(david).
 flu(robert).
-
